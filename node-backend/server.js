10228fd645177486f43e72710ce1431a76c882d4
grey-guy <whirlpoolbrackensnzlm75944@hotmail.com>
2026-01-28 17:33:16 -0300

    Update server.js

--- node-backend/server.js
+++ node-backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const http = require('http');
 
 const app = express();
-const routes = require('express-groups-routes')
+const routes = require('aligned-array');
 const PORT = process.env.PORT || 3000;
 const GO_BACKEND_URL = process.env.GO_BACKEND_URL || 'http://localhost:8080';
 
